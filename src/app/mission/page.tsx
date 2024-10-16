e2b0cc783e7ddc3420dc12a3a423373c4c350728
ZWang0987 <zwang577@ucr.edu>
2024-10-15 22:25:00 -0700

    removed temp header, but the image no longer uploads correctly

--- src/app/mission/page.tsx
+++ src/app/mission/page.tsx
@@ -3,13 +3,6 @@ import React from "react";
 const mission = () => {
   return (
     <div className="flex min-h-screen flex-col items-center bg-isa-beige-100 p-8">
-      {/* temp Header for when header gets done */}
-      <header className="mb-8 text-center">
-        <h1 className="text-5xl font-extrabold text-isa-bright-yellow">
-          HOLDER HEADER
-        </h1>
-      </header>
-
       {/* Image Section */}
       <section className="mb-8">
         <img
@@ -17,8 +10,6 @@ const mission = () => {
           className="aspect-w-16 aspect-h-9 w-full max-w-screen-xl rounded-lg object-cover shadow-lg"
         />
       </section>
-
-      {/* Mission Statement Text */}
       <section className="mb-8 mt-8 max-w-6xl px-8 text-center">
         <p className="text-4xl font-medium leading-normal text-isa-light-red">
           The Iranian Student Association at UCR aims to promote inclusivity and
