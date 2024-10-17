cc2ff2c8fe34825249e416fc46a4c0c299a02025
Matthew Martinez <matthewmartinez@ucr-secure-26-10-13-154-147.wnet.ucr.edu>
2024-10-17 11:26:55 -0700

    landing draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Text from "@/components/landing/landingtext";
 import React from "react";
 const Home = () => {
   return (
-    <div className = "h-screen bg-isa-dark-red">
+    <div className="h-screen bg-isa-dark-red">
       <Text />
     </div>
   );
--- src/components/landing/landingtext.tsx
+++ src/components/landing/landingtext.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 
-
 const text = () => {
   return (
     <>
@@ -9,19 +8,19 @@ const text = () => {
         <Image src={"/isalogo.png"} width={300} height={400} alt={"isalogo"} />
       </div>
       <div>
-        <h1 className="py-8 text-center text-4xl text-white font-sans">
+        <h1 className="py-8 text-center font-sans text-4xl text-white">
           Iranian Student Association
         </h1>
       </div>
       <div>
-        <p className="py-8 text-center text-4xl text-gray-500 font-serif">
-          Lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem 
-          </p>
+        <p className="py-8 text-center font-serif text-4xl text-gray-500">
+          Lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem
+        </p>
       </div>
       <div>
-        <p className="py-8 text-center text-4xl text-gray-500 font-serif">
+        <p className="py-8 text-center font-serif text-4xl text-gray-500">
           ipsumlorem ipsumv
-          </p>
+        </p>
       </div>
     </>
   );
