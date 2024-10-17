207fa3538ba5fdb575f18148961b80e52d9d1465
Matthew Martinez <matthewmartinez@Mac.home>
2024-10-16 22:03:09 -0700

    landing draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Text from "@/components/landing/landingtext";
 import React from "react";
 const Home = () => {
   return (
-    <div>
+    <div className = "h-screen bg-isa-dark-red">
       <Text />
     </div>
   );
--- src/components/landing/landingtext.tsx
+++ src/components/landing/landingtext.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import Image from "next/image";
+
 
 const text = () => {
-  return <div>text</div>;
+  return (
+    <>
+      <div>
+        <Image src={"/isalogo.png"} width={300} height={400} alt={"isalogo"} />
+      </div>
+      <div>
+        <h1 className="py-8 text-center text-4xl text-white font-sans">
+          Iranian Student Association
+        </h1>
+      </div>
+      <div>
+        <p className="py-8 text-center text-4xl text-gray-500 font-serif">
+          Lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem 
+          </p>
+      </div>
+      <div>
+        <p className="py-8 text-center text-4xl text-gray-500 font-serif">
+          ipsumlorem ipsumv
+          </p>
+      </div>
+    </>
+  );
 };
 
 export default text;
