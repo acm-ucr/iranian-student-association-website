1c0313fa969ac5258988fc06175098f02c597223
HaokunYang123 <t300025hao@gmail.com>
2024-10-17 12:05:45 -0700

    footer issues choppy

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image"
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Title
