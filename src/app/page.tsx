8abe38ae0af4e284685faea1960fac64769d340c
Matthew Martinez <matthewmartinez@Mac.home>
2024-10-16 20:26:14 -0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Text from "@/components/landing/landingtext";
+import React from "react";
 const Home = () => {
-  return <div className="bg-isa-dark-red">Hello World</div>;
+  return (
+    <div>
+      <Text />
+    </div>
+  );
 };
 
 export default Home;
