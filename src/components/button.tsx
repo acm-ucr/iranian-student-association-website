eedffdece1e34a186c65fb1bcec26c317c5e01c9
apaya22 <andypayan22@gmail.com>
2024-10-19 12:36:46 -0700

    Implemented next/link, made rounder

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
-const Button = () => {
+const Button = (props: { txt: string; link: string }) => {
   return (
-    <div>
-      <button className="inline-block rounded border-2 border-transparent bg-isa-light-red px-10 py-1 text-xl tracking-widest text-isa-bright-yellow">
-        Link
-      </button>
-    </div>
+    <Link
+      href={props.link}
+      className="box-border items-center justify-center rounded-full border-transparent bg-isa-light-red px-10 py-2 text-3xl"
+    >
+      <div className="relative inline-block whitespace-nowrap">
+        <span className="relative text-isa-bright-yellow">{props.txt}</span>
+      </div>
+    </Link>
   );
 };
 
