4e74d919f93aa8bbf69dc14a11d64d8636f2d71f
apaya22 <andypayan22@gmail.com>
2024-10-21 11:20:50 -0700

    ran prettier format

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,9 +8,7 @@ const Button = (props: { txt: string; link: string }) => {
       className="box-border items-center justify-center rounded-md border-transparent bg-isa-light-red px-11 py-2 text-3xl"
     >
       <div className="relative inline-block whitespace-nowrap">
-        <span className="relative font-normal text-isa-bright-yellow">
-          {props.txt}
-        </span>
+        <span className="relative text-isa-bright-yellow">{props.txt}</span>
       </div>
     </Link>
   );
