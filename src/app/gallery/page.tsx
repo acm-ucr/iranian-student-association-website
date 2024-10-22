a759e4e0cce1df061efa37ca0c425f066a46863d
dennyhly <dennyly626@gmail.com>
2024-10-22 00:39:08 -0700

    Moved components to their own files and shrunk paisleys and buttom

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,34 +1,12 @@
 import React from "react";
-import Image from "next/image";
-import Button from "@/components/gallery/buttons";
-import { Titles } from "@/data/gallery/Titles";
-import Paisley1 from "@/public/gallery/paisley1.svg";
-import Paisley2 from "@/public/gallery/paisley2.svg";
+import BackGround from "@/components/gallery/background";
+import ButtonLayout from "@/components/gallery/buttonLayout";
 
 const gallery = () => {
   return (
-    <div className="h-screen w-screen">
-      <div className="absolute top-1/4 -z-10">
-        <Image
-          src={Paisley1}
-          width={300}
-          height={300}
-          alt="First Paisley on Top Left of Page"
-        />
-      </div>
-      <div className="absolute right-0 top-full">
-        <Image
-          src={Paisley2}
-          width={350}
-          height={350}
-          alt="First Paisley on Top Left of Page"
-        />
-      </div>
-      <div className="h-full w-full">
-        {Titles.map((items, index) => (
-          <Button key={index} name={items.title} />
-        ))}
-      </div>
+    <div className="relative min-h-screen">
+        <ButtonLayout />
+        <BackGround />
     </div>
   );
 };
--- src/components/gallery/buttons.tsx
+++ src/components/gallery/buttons.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
 
 const Button = ({ name }: ButtonProps) => {
   return (
-    <div className="mx-auto my-20 flex h-2/6 w-3/5 items-center justify-center rounded-3xl border-2 border-isa-light-red bg-isa-light-gray text-6xl">
+    <div className="mx-auto my-5 flex h-1/5 w-5/12 items-center justify-center rounded-3xl border-2 border-isa-light-red bg-isa-light-gray text-6xl shrink">
       {name}
     </div>
   );
