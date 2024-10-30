1f07aadc2d112e0240041ac98f11508cb0bf86eb
HaokunYang123 <t300025hao@gmail.com>
2024-10-29 19:10:16 -0700

    fix build error 3

--- src/app/mission/page.tsx
+++ src/app/mission/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import MissionTitle from "@/public/Mission/Mission Statement.svg";
+import MissionTitle from "@/public/Mission/MissionStatement.svg";
 
 const page = () => {
   return (
