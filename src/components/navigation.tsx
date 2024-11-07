ceb4b2ef1791f4b142a316c30647d39cad490f47
ZWang0987 <zwang577@ucr.edu>
2024-11-07 13:03:36 -0800

    Attempt for nav bar responsiveness

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -20,16 +20,13 @@ const Navigation = () => {
               alt="Logo"
               width={50} // Default to 50px for smaller screens
               height={50}
-              className="rounded-full md:w-[75px] md:h-[75px] lg:w-[75px] lg:h-[75px]" // 75px on md, 75px on lg
+              className="rounded-full md:h-[75px] md:w-[75px] lg:h-[75px] lg:w-[75px]" // 75px on md, 75px on lg
             />
           </div>
         </div>
 
         {/* Responsive nav items */}
-        <ul className="flex flex-col sm:flex-row md:flex-row lg:flex-row 
-                       space-y-2 sm:space-y-0 md:space-y-0 lg:space-y-0 
-                       sm:space-x-4 md:space-x-6 lg:space-x-8 
-                       text-sm md:text-lg lg:text-xl font-semibold text-black">
+        <ul className="flex flex-col space-y-2 text-sm font-semibold text-black sm:flex-row sm:space-x-4 sm:space-y-0 md:flex-row md:space-x-6 md:space-y-0 md:text-lg lg:flex-row lg:space-x-8 lg:space-y-0 lg:text-xl">
           {items.map((item: item, index: number) => (
             <li key={index}>
               <Link href={item.href}>
