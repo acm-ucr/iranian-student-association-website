aa6029ddb0ba05a8f0e814687c41b1e38d45aa1f
VindenD <vinden.drummond@gmail.com>
2024-11-07 15:28:15 -0800

    Fixed title size

--- src/components/join-us/title.tsx
+++ src/components/join-us/title.tsx
@@ -4,7 +4,7 @@ import Title from "/public/join-us/JoinUs.svg";
 
 const title = () => {
   return (
-    <div className="max-w-screen relative flex-col justify-center text-center">
+    <div className="max-w-screen relative flex-col justify-center text-center scale-75">
       <Image src={Title} alt="Join Us Title" width={755} height={222}></Image>
     </div>
   );
