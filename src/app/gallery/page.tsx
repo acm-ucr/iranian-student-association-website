8d7e0eb711b80aa1e3eed8c4e25418b2636af2a7
dennyhly <dennyly626@gmail.com>
2024-11-07 16:11:47 -0800

    fixed responsiveness of title

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -6,11 +6,12 @@ import GalleryTitle from "@/public/gallery/Gallery.svg";
 
 const gallery = () => {
   return (
-    <div className="relative h-full ">
-      <div className="flex justify-center w-screen">
-        <Image src={GalleryTitle} 
-        alt="Our Gallery Page Title" 
-        className="w-4/6 md:w-1/2 md:mb-15 lg:mb-10 xl:w-5/12"
+    <div className="relative h-full">
+      <div className="flex w-screen justify-center">
+        <Image
+          src={GalleryTitle}
+          alt="Our Gallery Page Title"
+          className="md:mb-15 w-4/6 md:w-1/2 lg:mb-10 xl:w-5/12"
         />
       </div>
       <BackGround />
