5c20109b4de1efe88274d0344ee68cfaccbbc64a
VindenD <vinden.drummond@gmail.com>
2024-12-01 23:55:55 -0800

    fixed sizing

--- src/components/gallery/buttons.tsx
+++ src/components/gallery/buttons.tsx
@@ -9,7 +9,7 @@ const Button = ({ name, onClick }: ButtonProps) => {
   return (
     <div
       onClick={onClick}
-      className="mx-auto my-5 flex h-1/5 w-5/12 shrink cursor-pointer items-center justify-center rounded-3xl border-2 border-isa-light-red bg-isa-light-gray text-2xl transition-colors duration-300 hover:bg-isa-light-red hover:text-white sm:text-5xl md:text-6xl lg:text-6xl"
+      className="mx-auto my-5 flex h-1/5 w-5/12 shrink cursor-pointer items-center justify-center rounded-3xl border-2 border-isa-light-red bg-isa-light-gray text-2xl transition-colors duration-300 hover:bg-isa-light-red hover:text-white sm:text-4xl md:text-4xl lg:text-6xl"
     >
       {name}
     </div>
