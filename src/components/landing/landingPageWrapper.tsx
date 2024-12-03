993dc4cfcd9d7e6226b4af80fc58fac56d5246d1
Kavin Phabiani <kavinphab@gmail.com>
2024-12-02 18:53:19 -0800

    main background fix

--- src/components/landing/landingPageWrapper.tsx
+++ src/components/landing/landingPageWrapper.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-import "./landing.css";
 
 import Header from "./landingHeader";
 import LandingTitle from "./landingTitle";
 import LandingEvents from "./landingEvents";
+import HomeBG from "@/public/home/ucr.webp";
+import Image from "next/image";
 
 const landingPageWrapper = () => {
   return (
-    <div className="bg-landing min-h-screen">
+    <div className="relative min-h-screen">
+      <Image
+        src={HomeBG}
+        alt="Home Page BG"
+        className="absolute -z-10 h-full w-full object-cover"
+      />
       <Header />
       <LandingTitle />
       <LandingEvents />
