2a935b306e24c109166ebdd6da816fbbd63e9805
Briana Garcia <briana_garcia_@outlook.com>
2024-12-03 19:35:37 -0800

    Add fade from bottom, increase delay, and animate title

--- src/components/mission/page.tsx
+++ src/components/mission/page.tsx
@@ -6,22 +6,11 @@ import { motion } from "framer-motion";
 const animation1 = {
   hidden: {
     opacity: 0,
-    x: 80,
+    y: 20,
   },
   visible: {
     opacity: 1,
-    x: 0,
-  },
-};
-
-const animation2 = {
-  hidden: {
-    opacity: 0,
-    x: -80,
-  },
-  visible: {
-    opacity: 1,
-    x: 0,
+    y: 0,
   },
 };
 
@@ -29,7 +18,13 @@ const mission = () => {
   return (
     <div className="flex min-h-screen flex-col items-center p-8">
       <section className="mb-8 md:mb-16">
-        <div className="aspect-w-16 aspect-h-9 w-full max-w-6xl overflow-hidden rounded-lg shadow-lg">
+        <motion.div 
+          variants={animation1}
+          initial="hidden"
+          whileInView="visible"
+          transition={{ delay: 0.8 }}
+          className="aspect-w-16 aspect-h-9 w-full max-w-6xl overflow-hidden rounded-lg shadow-lg"
+          >
           <Image
             src="/mission/group.svg"
             alt="Group Mission"
@@ -38,15 +33,20 @@ const mission = () => {
             height={900}
             className="object-cover"
           />
-        </div>
+        </motion.div>
       </section>
       <section className="mb-8 mt-8 max-w-6xl px-8 text-center">
-        <p className="text-2xl font-medium leading-normal text-isa-light-red md:text-5xl lg:text-5xl">
-          The Iranian Student Association at UCR aims to promote inclusivity and
-          cultural awareness by organizing events that celebrate our shared
-          heritage, educate on political and historical topics, and foster a
-          united community where we can learn and grow together.
-        </p>
+        <motion.p 
+          variants={animation1}
+          initial="hidden"
+          whileInView="visible"
+          transition={{ delay: 0.8 }}
+          className="text-2xl font-medium leading-normal text-isa-light-red md:text-5xl lg:text-5xl">
+            The Iranian Student Association at UCR aims to promote inclusivity and
+            cultural awareness by organizing events that celebrate our shared
+            heritage, educate on political and historical topics, and foster a
+            united community where we can learn and grow together.
+        </motion.p>
       </section>
     </div>
   );
