94c131346ff6a321460755106138cd275cc8561f
Haokun Yang <haokunyang@Mac.lan>
2024-12-04 03:49:26 -0800

    format change

--- src/components/culture/text.tsx
+++ src/components/culture/text.tsx
@@ -52,7 +52,7 @@ const CultureText = () => {
           className="flex w-full flex-col items-center"
         >
           <div className="w-full px-5 sm:px-0 lg:px-20">
-            <div className="flex flex-col sm:flex-row w-full">
+            <div className="flex w-full flex-col sm:flex-row">
               <Image
                 className="mb-4 w-full sm:mr-4 sm:w-1/2 md:mb-0 md:mr-4 lg:w-1/2"
                 src={ShabeYalda}
@@ -72,19 +72,18 @@ const CultureText = () => {
           {/* animation */}
         </motion.div>
       </div>
-      <br></br> 
+      <br></br>
       <motion.div
         variants={animation}
         initial="hidden"
         whileInView="show"
         transition={{ duration: 0.8, delay: 0.6 }}
         className="px-8 text-center text-3xl text-isa-dark-red sm:px-52 lg:px-80"
-      > 
+      >
         <p>
           <b>Shabe Yalda Significance:</b> Shabe Yalda, also known as Yalda
           Night or the Winter Solstice celebration, is a significant and ancient
           Persian festival observed on the longest night of the year, which
-         
           typically falls on December 20th or 21st. This celebration marks the
           victory of light over darkness as the days begin to grow longer after
           the solstice. It is an event rich in symbolism and cultural importance
